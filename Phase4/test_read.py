ad8f18468b7a1a9e8d3dc5886590555188eca841
seyonr <seyonr@SR-Macrook.local>
2025-03-18 17:00:38 -0400

    use for read.py testing

---
+++ Phase4/test_read.py
@@ -0,0 +1,18 @@
+#### Acutal tester for the number of lines of code ####
+# from read import read_old_bank_accounts
+
+# def main():
+#     accounts = read_old_bank_accounts("accounts.txt")
+#     print("\nValid accounts parsed:")
+#     for account in accounts:
+#         print(account)
+
+# if __name__ == "__main__":
+#     main()
+
+
+#### Use to find the count of the lines for each character ####
+# with open("accounts.txt", "r") as f:
+#     for i, line in enumerate(f, start=1):
+#         # Remove the newline character and count the rest
+#         print("Line {}: {} characters".format(i, len(line.rstrip('\n'))))
