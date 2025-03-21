d38cf692a9024277f547487657f2f852ae5714f7
seyonr <seyonr@SR-Macrook.local>
2025-03-21 12:17:31 -0400

    small logical changes

--- Phase4/delete.py
+++ Phase4/delete.py
@@ -1,19 +1,13 @@
 from print_error import log_constraint_error
+from read import read_old_bank_accounts
+from write import write_new_current_accounts
 
 class Delete:
-
-    def __init__(self, amount, account_num, is_admin, plan):
-        self.amount = amount
+    def __init__(self, account_num, is_admin):
         self.account_num = account_num
         self.is_admin = is_admin
-        self.plan = plan
-
 
     def delete(self):
-        from read import read_old_bank_accounts
-        from write import write_new_current_accounts
-
-
         file_path = ""  # Set to correct path file
         accounts = read_old_bank_accounts(file_path) 
         
@@ -25,5 +19,5 @@ class Delete:
                 write_new_current_accounts(accounts, file_path)
                 return
         else:
-            log_constraint_error("Account Violation Error", "Account does not exisit")
+            log_constraint_error("Unprivileged User Error", "Standard user does not have admin privileges")
 
--- Phase4/disable.py
+++ Phase4/disable.py
@@ -1,38 +1,31 @@
 from print_error import log_constraint_error
+from read import read_old_bank_accounts
+from write import write_new_current_accounts
 
 class Disable:
 
-    def __init__(self, amount, account_num, is_admin, plan):
-        self.amount = amount
+    def __init__(self, account_num, is_admin):
         self.account_num = account_num
         self.is_admin = is_admin
-        self.plan = plan
-
-        
-
 
     def disable(self):
-        from read import read_old_bank_accounts
-        from write import write_new_current_accounts
-
-
         file_path = ""  # Set to correct path file
         accounts = read_old_bank_accounts(file_path) 
         
         if(self.is_admin): # Admin code handle
             for x in (accounts):
-
                 if(int(x['account_number']) == self.account_num):
-                    if ((x['status'])=="A"):
-                        x['status']="D"
+                    if ((x['status']) == "A"):
+                        x['status'] = "D"
                     else:
                         x['status']="A"
+                    
+                    write_new_current_accounts(accounts, file_path)
                 
                 else:
                     log_constraint_error("Account Violation Error", "Account does not exisit")
-     
-                write_new_current_accounts(accounts, file_path)
+                
                 return
         else:
-            log_constraint_error("Account Violation Error", "Account does not exisit")
+           log_constraint_error("Unprivileged User Error", "Standard user does not have admin privileges")
 
