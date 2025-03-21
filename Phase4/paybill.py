6e8fe8b522a0c262792201e83bcc2f7e41a7beba
seyonr <seyonr@SR-Macrook.local>
2025-03-21 11:54:46 -0400

    write to file added for paybill

--- Phase4/paybill.py
+++ Phase4/paybill.py
@@ -1,4 +1,6 @@
 from print_error import log_constraint_error
+from read import read_old_bank_accounts
+from write import write_new_current_accounts
 
 class Paybill:
     def __init__(self, amount, account_num, is_admin):
@@ -8,8 +10,6 @@ class Paybill:
 
     # Pass the accounts data to another function or file
     def paybill(self):
-        from read import read_old_bank_accounts
-
         file_path = "" # Set to correct path file 
         accounts = read_old_bank_accounts(file_path) # Will hold all valid accounts
         
@@ -20,6 +20,7 @@ class Paybill:
                         log_constraint_error("Balance Violation Error", "Insufficent balance")
                     else:
                         x["balance"] -= self.amount
+                        write_new_current_accounts(accounts, file_path)
                 else:
                     log_constraint_error("Account Violation Error", "Account does not exisit")
 
@@ -27,13 +28,14 @@ class Paybill:
             for x in accounts:
                 if(int(x['account_number']) == self.account_num):
                     if(self.amount > 2000):
-                        log_constraint_error("Withdrawl Limit Violation", "Standard user cannot withdraw above $1000")
+                        log_constraint_error("Withdrawl Limit Violation", "Standard user cannot withdraw above $2000")
                     
                     elif(self.amount > int(x["balance"])):
                         log_constraint_error("Balance Violation Error", "Insufficent balance")
 
                     else:
                         x["balance"] -= self.amount
+                        write_new_current_accounts(accounts, file_path)
                 else:
                     log_constraint_error("Account Violation Error", "Account does not exisit")
         
